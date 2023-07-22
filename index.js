da5415b74b145e2705292bac3c047b24d45ff286
kanyinsola Adesida <87780625+technurse911@users.noreply.github.com>
2023-07-22 10:13:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+window.addEventListener('load', ()=>{
+    let long;
+    let lat;
+    let locationTimezone = document.querySelector("#locationTimezone")
+    let tempDescription = document.querySelector("#tempDescription");
+    let tempDegree = document.querySelector("#tempDegree");
+    let unit = document.querySelector("#unit");
+
+
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(position  =>{
+        console.log(position);
+        long = position.coords.longitude;
+        lat = position.coords.latitude;
+        
+        // Api
+      const api = `http://api.weatherapi.com/v1/current.json?key=2c1e1615efc54dbf9cb152609231403&q= ${lat},${long}&aqi=no`
+      
+      fetch(api)
+         .then(response =>{
+            return response.json();
+      })
+      .then(data =>{
+        console.log(data);
+
+        locationTimezone.textContent = data.location.tz_id
+        tempDescription.textContent = "It's "  + data.current.condition.text;
+        tempDegree.textContent = `${data.current.temp_f}F   /  ${data.current.temp_c}C`;
+        
+    //    tempDegree.addEventListener ("click", ()=>{
+    //       if(unit.textContent === "F"){
+    //         tempDegree.textContent = data.current.temp_c;
+    //         unit.textContent = "C"
+    //       } else{
+    //         unit.textContent = "F"
+    //         tempDegree.textContent = data.current.temp_f
+    //       }
+    //    })
+
+    
+      })
+      })
+  
+    }
+    console.log('working')
+  })
